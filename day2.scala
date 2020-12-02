aa08aa98007285708d7746b12c9dd7312421433b
michael1241 <michael@jamcupboard.co.uk>
2020-12-02 19:03:31 +0000

    using sbt and better type safety

--- day2.scala
+++ day2.scala
@@ -1,31 +1,36 @@
 import scala.io.Source
 import scala.util.matching.Regex
 
+object Day2 extends App {
+    case class Line(low: Int, high: Int, char: Char, password: String)
 
-case class Line(low: Int, high: Int, char: Char, password: String)
-
-def readFile(line: String): Option[Line] = {
-    val info = "^([0-9]+)-([0-9]+) (.): ([^\\s]+)$".r
-    line match {
-        case info(low, high, char, password) => Some(Line(low.toInt, high.toInt, char.head, password))
-        case _ => None
+    def readFile(line: String): Option[Line] = {
+        val info = "^([0-9]+)-([0-9]+) (.): ([^\\s]+)$".r
+        line match {
+            case info(low, high, char, password) =>
+                for {
+                    low <- low.toIntOption
+                    high <- high.toIntOption
+                    char <- char.lift(0)
+                } yield Line(low, high, char, password)
+            case _ => None
+        }
     }
+
+    val inputlines = Source.fromFile("day2in").getLines.toList
+
+    val lines = inputlines.flatMap(readFile(_))
+
+    def isValid(line: Line): Boolean = {
+        val count = line.password.count(_ == line.char)
+        (count >= line.low) && (count <= line.high)
+    }
+
+    def isValid2(line: Line): Boolean = {
+        (line.password.charAt(line.low -1) == line.char) ^ (line.password.charAt(line.high -1) == line.char)
+    }
+
+    println(lines.count(isValid(_)))
+
+    println(lines.count(isValid2(_)))
 }
-
-val inputlines = Source.fromFile("day2in").getLines.toList
-
-val Lines = inputlines.map(readFile(_))
-
-def isValid(line: Line): Boolean = {
-    val count = line.password.count(_ == line.char)
-    (count >= line.low) && (count <= line.high)
-}
-
-def isValid2(line: Line): Boolean = {
-    (line.password.charAt(line.low -1) == line.char) ^ (line.password.charAt(line.high -1) == line.char)
-}
-
-println(Lines.flatten.count(isValid(_)))
-
-println(Lines.flatten.count(isValid2(_)))
-
