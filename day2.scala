01850a16944ccf6b65ce7cac71e65953ec434258
michael1241 <michael@jamcupboard.co.uk>
2020-12-02 17:40:49 +0000

    neater

--- day2.scala
+++ day2.scala
@@ -1,21 +1,31 @@
 import scala.io.Source
+import scala.util.matching.Regex
 
-val lines = Source.fromFile("day2in").getLines.toList
 
-def isValid(line: String): Boolean = {
-    val range :: char :: password :: _ = line.split("[ ]").toList
-    val low :: high :: _ = range.split("-").toList
-    val count = password.count(_ == char.charAt(0)) 
-    (count >= low.toInt) && (count <= high.toInt)
+case class Line(low: Int, high: Int, char: Char, password: String)
+
+def readFile(line: String): Option[Line] = {
+    val info = "^([0-9]+)-([0-9]+) (.): ([^\\s]+)$".r
+    line match {
+        case info(low, high, char, password) => Some(Line(low.toInt, high.toInt, char.head, password))
+        case _ => None
+    }
 }
 
-def isValid2(line: String): Boolean = {
-    val range :: c :: password :: _ = line.split("[ ]").toList
-    val low :: high :: _ = range.split("-").toList
-    val char = c.charAt(0)
-    ((password.charAt(low.toInt -1) == char) && (password.charAt(high.toInt -1) != char) || (password.charAt(low.toInt -1) != char) && (password.charAt(high.toInt -1) == char))
+val inputlines = Source.fromFile("day2in").getLines.toList
+
+val Lines = inputlines.map(readFile(_))
+
+def isValid(line: Line): Boolean = {
+    val count = line.password.count(_ == line.char)
+    (count >= line.low) && (count <= line.high)
 }
 
-lines.filter(isValid2).length
+def isValid2(line: Line): Boolean = {
+    ((line.password.charAt(line.low -1) == line.char) && (line.password.charAt(line.high -1) != line.char) || (line.password.charAt(line.low -1) != line.char) && (line.password.charAt(line.high -1) == line.char))
+}
 
+println(Lines.flatten.count(isValid(_)))
+
+println(Lines.flatten.count(isValid2(_)))
 
