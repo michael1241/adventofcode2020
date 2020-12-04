e3f75054d7b95c524186ff5677bca8663c62dae9
michael1241 <michael@jamcupboard.co.uk>
2020-12-04 16:10:26 +0000

    finished day4 part2

--- day4.scala
+++ day4.scala
@@ -1,4 +1,5 @@
 import scala.io.Source
+import scala.util.matching.Regex
 
 object Day4 extends App {
     def parseLine(l: String): Map[String, String] =
@@ -6,7 +7,20 @@ object Day4 extends App {
 
     def valid(line: Map[String, String]): Boolean = {
         val keys: Set[String] = Set("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
-        (keys.subsetOf(line.keySet))
+
+        if (!keys.subsetOf(line.keySet)) return false
+
+        val hgt: Int = line("hgt").filter(_.isDigit).toInt
+        val hgttype: String = line("hgt").filter(_.isLetter).toString
+        (
+            line("byr").toInt >= 1920 && line("byr").toInt <= 2002 &&
+            line("iyr").toInt >= 2010 && line("iyr").toInt <= 2020 &&
+            line("eyr").toInt >= 2020 && line("eyr").toInt <= 2030 &&
+            ((hgttype == "cm" && hgt >= 150 && hgt <= 193) || (hgttype == "in" && hgt >= 59 && hgt <= 76)) &&
+            line("hcl").matches("#[0-9a-f]{6}") &&
+            line("ecl").matches("amb|blu|brn|gry|grn|hzl|oth") &&
+            line("pid").matches("[0-9]{9}")
+        )
     }
 
     val data: Array[String] = Source.fromFile("day4in").mkString.split("\\n\\n").map(_.replace("\n"," "))
