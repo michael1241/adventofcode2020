132d74f1c3eff7742cc8530c6378ebb708b3ccc1
michael1241 <michael@jamcupboard.co.uk>
2020-12-05 15:46:55 +0000

    day 4 refactor start

--- day4.scala
+++ day4.scala
@@ -2,28 +2,33 @@ import scala.io.Source
 import scala.util.matching.Regex
 
 object Day4 extends App {
+    case class Person(byr: Int, iyr: Int, eyr: Int, hgt: String, hcl: String, ecl: String, pid: Int)
+
     def parseLine(l: String): Map[String, String] =
         l.split(' ').map(_ split ':').collect { case Array(k, v) => (k, v) }.toMap
 
-    def valid(line: Map[String, String]): Boolean = {
+    def makePerson(l: Map[String, String]): Option[Person] = {
+        for {
+            byr <- l("byr").toIntOption
+            iyr <- l("iyr").toIntOption
+            eyr <- l("eyr").toIntOption
+            hgt <- l("hgt")
+            hcl <- l("hcl")
+            ecl <- l("ecl")
+            pid <- l("pid").toIntOption
+        } yield Some(Person(byr, iyr, eyr, hgt, hcl, ecl, pid))
+    }
+
+    def validateFields(l: Map[String, String]): Option[Person] = {
         val keys: Set[String] = Set("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
-
-        if (!keys.subsetOf(line.keySet)) return false
-
-        val hgt: Int = line("hgt").filter(_.isDigit).toInt
-        val hgttype: String = line("hgt").filter(_.isLetter).toString
-        (
-            line("byr").toInt >= 1920 && line("byr").toInt <= 2002 &&
-            line("iyr").toInt >= 2010 && line("iyr").toInt <= 2020 &&
-            line("eyr").toInt >= 2020 && line("eyr").toInt <= 2030 &&
-            ((hgttype == "cm" && hgt >= 150 && hgt <= 193) || (hgttype == "in" && hgt >= 59 && hgt <= 76)) &&
-            line("hcl").matches("#[0-9a-f]{6}") &&
-            line("ecl").matches("amb|blu|brn|gry|grn|hzl|oth") &&
-            line("pid").matches("[0-9]{9}")
-        )
+        keys.subsetOf(l.keySet) match {
+            case true => makePerson(l)
+            case _ => None
+        }
     }
 
     val data: Array[String] = Source.fromFile("day4in").mkString.split("\\n\\n").map(_.replace("\n"," "))
     val parsed: Array[Map[String, String]] = data map parseLine
-    println(parsed count valid)
+    val validated: Array[Person] = parsed flatMap validateFields
+    println(validated.length)
 }
