b87934d7f22a7613d8fcda31998b38b7e1c3ccf0
michael1241 <michael@jamcupboard.co.uk>
2020-12-05 11:48:45 +0000

    day5 refactor simplified

--- day5.scala
+++ day5.scala
@@ -3,10 +3,7 @@ import scala.language.postfixOps
 import java.lang.Integer
 
 object Day5 extends App {
-    def separate(ticket: String): (String, String) =
-        (ticket.substring(0,7), ticket.substring(7,10))
-
-    def convert(ticket: (String, String)): (Int, Int) = {
+    def convert(ticket: String): Int = {
         def letterToDigit(letter: Char): Char = {
             letter match {
                 case 'F' => '0'
@@ -15,25 +12,15 @@ object Day5 extends App {
                 case 'R' => '1'
             }
         }
-        ticket match {
-            case (row, col) => (Integer.parseInt(row map letterToDigit, 2), Integer.parseInt(col map letterToDigit, 2))
-        }
-    }
-
-    def seatID(ticket: (Int, Int)): Int = {
-        ticket match {
-            case (row, col) => (row * 8) + col
-        }
+    Integer.parseInt((ticket map letterToDigit), 2)
     }
 
     val input: List[String] = Source.fromFile("day5in").getLines.toList
-    val separated: List[(String, String)] = input map separate
-    val converted: List[(Int, Int)] = separated map convert
-    val seatIDs: List[Int] = converted map seatID
-    println(seatIDs.max)
+    val converted: List[Int] = input map convert
+    println(converted.max)
 
-    val actuals = seatIDs.toSet
-    val possibles = (seatIDs.min to seatIDs.max).toSet
+    val actuals = converted.toSet
+    val possibles = (converted.min to converted.max).toSet
 
     println(possibles &~ actuals)
 }
