e885c64435011849a720b34ddb24ba7ae53ed130
michael1241 <michael@jamcupboard.co.uk>
2020-12-06 12:08:10 +0000

    day 6 part 2 removing part 1

--- day6.scala
+++ day6.scala
@@ -3,9 +3,7 @@ import scala.io.Source
 object Day6 extends App {
     val data: Array[String] = Source.fromFile("day6in").mkString.split("\\n\\n")
     val groups: Array[Array[Set[String]]] = data.map(_.split("\\n")).map(_.map(_.split("").toSet))
-    
-    val counts: Array[Int] = data.map(_.size)
-    
+
     def grouptransform(group: Array[Set[String]]): Int = {
         group.reduce(_.intersect(_)).size
     }
