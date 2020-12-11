85013e9de2e4f9f94c3d26902374b033476901b5
michael1241 <michael@jamcupboard.co.uk>
2020-12-11 11:22:43 +0000

    WIP day11

--- day11.scala
+++ day11.scala
@@ -4,15 +4,22 @@ object Day11 extends App {
 
     case class Seat(x: Int, y: Int, occupied: Boolean)
 
-    def validate(c: Char): Option[Char] =
-        c match {
-            case '#' => Some('#')
-            case 'L' => Some('L')
-            case '.' => Some('.')
-            case _ => None
+    def validate(x: Int, y: Int, char: String): Option[Seat] = {
+            char match {
+                case "#" => Some(Seat(x, y, true))
+                case "L" => Some(Seat(x, y, false))
+                case _ => None
+            }
         }
 
-    val data: Vector[Vector[Char]] = Source.fromFile("day11in").mkString.split("\\n").toVector.map(_.flatMap(validate(_)).toVector)
+    val data: Vector[Vector[String]] = Source.fromFile("day11in").mkString.split("\\n").toVector.map(_.split("").toVector)
 
-    data map println
+    //data.zipWithIndex.map{case (digit, index) => ((index / 7, index % 7), digit.asDigit)}.toMap
+    val seats: Set[Seat] = { for {
+            y <- 0 to data.length - 1
+            x <- 0 to data(0).length - 1
+        } yield validate(x, y, data(y)(x))
+    }.flatten.toSet
+
+    println(seats)
 }
