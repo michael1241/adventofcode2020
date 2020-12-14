4848ea472df61499d4c7ee47fe1cf40f2cc1cd76
michael1241 <michael@jamcupboard.co.uk>
2020-12-14 20:38:08 +0000

    day 14 part 1

--- day14.scala
+++ day14.scala
@@ -1,7 +1,7 @@
 import scala.io.Source
 
 object Day14 extends App {
-    case class Instruction(address: Int, value: Int)
+    case class Instruction(address: Int, value: BigInt)
 
     def translate(char: Char): Option[Int] = {
         char match {
@@ -18,11 +18,9 @@ object Day14 extends App {
     def parse(input: String): Either[List[Option[Int]], Instruction] = {
         input match {
             case s"mask = ${mask}" => Left(toMask(mask))
-            case s"mem[${address}] = ${value}" => Right(Instruction(address.toIntOption.getOrElse(0), value.toIntOption.getOrElse(0)))
+            case s"mem[${address}] = ${value}" => Right(Instruction(address.toIntOption.getOrElse(0), BigInt(value)))
         }
     }
-
-    val data: List[Either[List[Option[Int]], Instruction]] = Source.fromFile("day14in").mkString.split("\\n").toList.map(parse(_))
 
     def compare(valbit: Option[Int], maskbit: Option[Int]): Int = {
         maskbit match {
@@ -31,29 +29,30 @@ object Day14 extends App {
         }
     }
 
-    def applyMask(value: Int, mask: List[Option[Int]]): Int = {
-        val binvalue: List[Option[Int]] = {value.toBinaryString map translate}.toList
-        for {
-            (valbit, maskbit) <- binvalue.reverse.zipAll(mask.reverse, Some(0), Some(0))
-        }
-        //println(valbit, maskbit)
-        println(compare(valbit, maskbit))
-        1
-    }
-    applyMask(17, List(None, None, None, None, None, None))
-
-    def addElement(memory: Map[Int,Int], instr: Instruction, mask: List[Option[Int]]): Map[Int, Int] = {
-        val newMemory: Map[Int, Int] = Map(instr.address -> applyMask(instr.value, mask))
-        (memory ++ newMemory)
+    def applyMask(value: BigInt, mask: List[Option[Int]]): BigInt = {
+        val binvalue: List[Option[Int]] = {value.toString(2) map translate}.toList
+        val binstring: String = binvalue.reverse.zipAll(mask.reverse, Some(0), Some(0)).map { case (a, b) => compare(a, b) }.reverse.mkString
+        BigInt(binstring, 2)
     }
 
-    def process(data: List[Either[List[Option[Int]], Instruction]], mask: List[Option[Int]], memory: Map[Int, Int]): Map[Int, Int] = {
+    def addElement(memory: Map[Int, BigInt], instr: Instruction, mask: List[Option[Int]]): Map[Int, BigInt] = {
+        val newMemory: Map[Int, BigInt] = memory ++ Map(instr.address -> applyMask(instr.value, mask))
+        newMemory
+    }
+
+    def process(data: List[Either[List[Option[Int]], Instruction]], mask: List[Option[Int]], memory: Map[Int, BigInt]): Map[Int, BigInt] = {
         data match {
             case Nil => memory
             case Left(head) :: tail => process(tail, head, memory)
             case Right(head) :: tail =>
-                val updatedmemory: Map[Int,Int] = addElement(memory, head, mask)
+                val updatedmemory: Map[Int,BigInt] = addElement(memory, head, mask)
                 process(tail, mask, updatedmemory)
         }
     }
+
+    val data: List[Either[List[Option[Int]], Instruction]] = Source.fromFile("day14in").mkString.split("\\n").toList.map(parse(_))
+    val initMask: List[Option[Int]] = List(None)
+    val initMemory: Map[Int, BigInt] = Map()
+
+    println(process(data, initMask, initMemory).foldLeft(0: BigInt)(_+_._2) )
 }
